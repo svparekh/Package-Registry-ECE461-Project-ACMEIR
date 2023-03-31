030574ce501d21e4b600482dd6ed421ec1495a74
Chase McCoy <chasemccoy@Chases-MacBook-Air.local>
2023-03-31 18:15:34 -0400

    more work on query packages

--- src/lambda_functions/get_packages.py
+++ src/lambda_functions/get_packages.py
@@ -3,35 +3,57 @@ import requests
 
 def lambda_handler(event, context):
   
-  url = "https://firestore.googleapis.com/v1/projects/acme-register/databases/(default)/documents/existing-versions-by-name/"
+  # get packages
+  url = "https://firestore.googleapis.com/v1/projects/acme-register/databases/(default)/documents/packages/"
   response = requests.get(url).json()
-  documents = response['documents']
+  packages = response['documents']
   
-  name_to_version = [] # {name1: [version1, version2]}
-  for document in documents:
-    name = document["name"][79:]
-    name_to_version[name] = []
-    
-    versions = document["fields"]["versions"]["arrayValue"]["values"] # [{'stringValue': '1.2.3'}, {'stringValue': '1.2.9'}]
-    for version in versions:
-      name_to_version[name].append(version['stringValue'])
-
-  queries = [] # filling in an array of queries - each query is in format of tuple (name, version)
-  for query in event:
-    queries.append((query['Name'], query["Version"]))
-
-  packages_that_fit_query = []
-  for query in queries:
-    if query[0] == "*":
-      if query[1][0] == '~':
-        characters_the_version_must_have = query[1][1:-1]
-        for name in name_to_version:
-          if name
-      elif query[1][0] == '^':
-        
-      elif len(query[1]) == 5: # straight version
-      
-      else: # range of versions
-        
+  # establish name->version dictionary
+  name_to_version = {} # {name1: [version1, version2]}
+  for package in packages:
+    package_name = package['fields']['name']['stringValue']
+    package_version = package['fields']['version']['stringValue']
+    if package_name in name_to_version:
+      name_to_version[package_name].append[package_version]
     else:
-        
+      name_to_version[package_name] = [package_version]
+      
+  # get target package name(s) and version(s)
+  target_package_versions = {} # {name1: (1.2.3, 1.2.4, 1.2.5), name2: (1.2.3, 1.2.4, 1.2.5)}
+  for query in event:
+    query_name = query['Name']
+    query_version = query['Version']
+    
+    if query_name not in target_package_versions:
+      target_package_versions[query_name] = set()
+    
+    if len(query_version) == 5: # exact version
+      target_package_versions[query_name].add(query_version)
+    elif query_version[0] == '^': # carat
+      target_package_versions[query_name].update(getCaratVersionNumbers(query_version))
+    elif query_version[0] == '~': # tilde
+      target_package_versions[query_name].update(getTildeVersionNumbers(query_version))
+    else: # bounded range
+    
+def getCaratVersionNumbers(startingVersion):
+  version_numbers = set()
+  patch_number = int(startingVersion[-1])
+  minor_number = int(startingVersion[-3])
+  while minor_number < 10:
+    while patch_number < 10:
+      version_numbers.add(startingVersion[:-3] + str(minor_number) + '.' + str(patch_number))
+      patch_number += 1
+    minor_number += 1
+    patch_number = 1
+  return version_numbers
+
+
+def getTildeVersionNumbers(startingVersion):
+  version_numbers = set()
+  patch_number = int(startingVersion[-1])
+  while patch_number < 10:
+    version_numbers.add(startingVersion[:-1] + str(patch_number))
+    patch_number += 1
+    
+  return version_numbers
+  
