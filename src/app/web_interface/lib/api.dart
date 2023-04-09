d2e60e1412c4469501325901fd9763c0d4587d62
svparekh <setulvp@gmail.com>
2023-04-09 18:15:58 -0400

    Update dependencies

--- src/app/web_interface/lib/api.dart
+++ src/app/web_interface/lib/api.dart
@@ -1,9 +1,22 @@
+import 'package:http/http.dart' show get;
+
 class APICaller {
   static String baseUrl = "";
-  static String endpoint = "$baseUrl/";
+  static String addEndpoint = "$baseUrl/";
+  static String deleteEndpoint = "$baseUrl/";
+  static String updateEndpoint = "$baseUrl/";
 
-  static addPackage({required String url}) {
+  static addPackage({required String url}) async {
     print("added $url");
+    try {
+      var url = Uri.parse(baseUrl + addEndpoint);
+      var response = await get(url);
+      if (response.statusCode == 200) {
+        // Something
+      }
+    } catch (e) {
+      print(e);
+    }
     // Must check if package with same name and version already exists or not
   }
 
