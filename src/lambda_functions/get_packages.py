f3e6d004a2115fa031a896d7dcc316a3d9694137
Chase McCoy <chasemccoy@Chases-MacBook-Air.local>
2023-04-09 20:41:22 -0400

    mostly finished get-packages. Still needs testing and pagination

--- src/lambda_functions/get_packages.py
+++ src/lambda_functions/get_packages.py
@@ -2,9 +2,10 @@ import json
 import requests
 
 def lambda_handler(event, context):
-  
-  # if you can see this, CI/CD works 3
 
+  # Needs to be tested extensively. I've tested a decent amount, but there's like 100 possible test cases. I
+  # might make a generate test case  script 
+  
   # get packages
   url = "https://firestore.googleapis.com/v1/projects/acme-register/databases/(default)/documents/packages/"
   response = requests.get(url).json()
@@ -19,39 +20,80 @@ def lambda_handler(event, context):
       name_to_version[package_name].append[package_version]
     else:
       name_to_version[package_name] = [package_version]
-      
-  # get target package name(s) and version(s)
-  target_package_versions = {} # {name1: (1.2.3, 1.2.4, 1.2.5), name2: (1.2.3, 1.2.4, 1.2.5)}
+
+  # get matching packages
+  matching_packages = []
   for query in event:
     query_name = query['Name']
     query_version = query['Version']
     
-    if query_name not in target_package_versions:
-      target_package_versions[query_name] = set()
-    
     if len(query_version) == 5: # exact version
-      target_package_versions[query_name].add(query_version)
+      if query_name == "*":
+        for name in name_to_version:
+          for version in name_to_version[name]:
+            if version == query_version:
+              matching_packages.append((name, version))
+      else:
+          for version in name_to_version[query_name]:
+            if version == query_version:
+              matching_packages.append((query_name, version))
+
     elif query_version[0] == '^': # carat
-      target_package_versions[query_name].update(getCaratVersionNumbers(query_version))
+      query_version = query_version[1:]
+      if query_name == "*":
+        for name in name_to_version:
+          for version in name_to_version[name]:
+            if fitsCaratTarget(query_version, version):
+              matching_packages.append((name, version))
+      else:
+          for version in name_to_version[query_name]:
+            if fitsCaratTarget(query_version, version):
+              matching_packages.append((query_name, version))
+
     elif query_version[0] == '~': # tilde
-      target_package_versions[query_name].update(getTildeVersionNumbers(query_version))
+      query_version = query_version[1:]
+      if query_name == "*":
+        for name in name_to_version:
+          for version in name_to_version[name]:
+            if fitsTildeTarget(query_version, version):
+              matching_packages.append((name, version))
+      else:
+          for version in name_to_version[query_name]:
+            if fitsTildeTarget(query_version, version):
+              matching_packages.append((query_name, version))
+
     else: # bounded range
-    
+      query_version_low = query_version[0:5]
+      query_version_high = query_version[6:]
+      if query_name == "*":
+        for name in name_to_version:
+          for version in name_to_version[name]:
+            if fitsBoundedRangeTarget(query_version_low, query_version_high, version):
+              matching_packages.append((name, version))
+      else:
+          for version in name_to_version[query_name]:
+            if fitsBoundedRangeTarget(query_version_low, query_version_high, version):
+              matching_packages.append((query_name, version))
+
+  # return in proper format
+  matching_packages = [{"Version" : v, "Name" : n} for (n, v) in matching_packages]
+  return json.dumps(matching_packages)
+
 def fitsTildeTarget(target, version):
-  return version[0] == target[0] and version[2] == target[2] and version[4] >= target[4]
+  return int(version[0]) == int(target[0]) and int(version[2]) == int(target[2]) and int(version[4]) >= int(target[4])
 
 def fitsCaratTarget(target, version):
 
-  same_minor_higher_or_equal_patch = version[0] == target[0] and version[2] == target[2] and version[4] >= target[4]
-  higher_minor = version[0] == target[0] and version[2] > target[2]
+  same_minor_higher_or_equal_patch = int(version[0]) == int(target[0]) and int(version[2]) == int(target[2]) and int(version[4]) >= int(target[4])
+  higher_minor = int(version[0]) == int(target[0]) and int(version[2]) > int(target[2])
 
   return same_minor_higher_or_equal_patch or higher_minor
 
 def fitsBoundedRangeTarget(targetLow, targetHigh, version):
-  major_in_between_targets_non_inclusive = version[0] < targetHigh[0] and version[0] > targetLow[0]
-  major_is_low_and_same_minor_higher_or_equal_patch = version[0] == targetLow[0] and version[2] == targetLow[2] and version[4] >= targetLow[4]
-  major_is_low_and_higher_minor = version[0] == targetLow[0] and version[2] > targetLow[2]
-  major_is_high_and_same_minor_lower_or_equal_patch = version[0] == targetHigh[0] and version[2] == targetHigh[2] and version[4] <= targetHigh[4]
-  major_is_high_and_lower_minor = version[0] == targetHigh[0] and version[2] < targetHigh[2]
+  major_in_between_targets_non_inclusive = int(version[0]) < int(targetHigh[0]) and int(version[0]) > int(targetLow[0])
+  major_is_low_and_same_minor_higher_or_equal_patch = int(version[0]) == int(targetLow[0]) and int(version[2]) == int(targetLow[2]) and int(version[4]) >= int(targetLow[4])
+  major_is_low_and_higher_minor = int(version[0]) == int(targetLow[0]) and int(version[2]) > int(targetLow[2])
+  major_is_high_and_same_minor_lower_or_equal_patch = int(version[0]) == int(targetHigh[0]) and int(version[2]) == int(targetHigh[2]) and int(version[4]) <= int(targetHigh[4])
+  major_is_high_and_lower_minor = int(version[0]) == int(targetHigh[0]) and int(version[2]) < int(targetHigh[2])
   
   return major_in_between_targets_non_inclusive or major_is_low_and_same_minor_higher_or_equal_patch or major_is_low_and_higher_minor or major_is_high_and_same_minor_lower_or_equal_patch or major_is_high_and_lower_minor
