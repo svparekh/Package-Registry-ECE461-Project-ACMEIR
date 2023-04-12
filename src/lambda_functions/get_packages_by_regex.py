ad49fdc02ca58f75eb9203a4de81ef38ea371a6d
Chase McCoy <chasemccoy@Chases-MacBook-Air.local>
2023-04-12 16:28:19 -0400

    initial regex lambda code

---
+++ src/lambda_functions/get_packages_by_regex.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import re
+
+def lambda_handler(event, context):
+  
+    package_regex = event['path'][17:] # /package/byregex/_______
+
+    url = "https://firestore.googleapis.com/v1/projects/acme-register/databases/(default)/documents/packages/"
+
+    documents = requests.get(url).json()
+    
+    matching_packages = []
+    for document in documents:
+        package_name = document['fields']['package_name']['stringValue']
+        package_version = document['fields']['package_version']['stringValue']
+        package_id = document['fields']['package_id']['stringValue']
+
+        if nameMatchesRegex(package_name, package_regex):
+            matching_packages.append({"Name" : package_name, "Version" : package_version, "ID" : package_id})
+
+    proxy_integration_response = {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "body": json.dumps(matching_packages),
+    }
+
+    return proxy_integration_response
+  
+
+def nameMatchesRegex(name, regex):
+    expression = re.compile(regex)
+    return expression.match(name) != None
