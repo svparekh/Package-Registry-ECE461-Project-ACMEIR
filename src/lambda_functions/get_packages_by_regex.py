3bff4d2f59f6bbf35bd51871aeb1b22523a37b1b
Chase McCoy <chasemccoy@Chases-MacBook-Air.local>
2023-04-17 18:50:41 -0400

    updated responses for regex lambda

--- src/lambda_functions/get_packages_by_regex.py
+++ src/lambda_functions/get_packages_by_regex.py
@@ -5,30 +5,38 @@ import re
 def lambda_handler(event, context):
   
     package_regex = event['RegEx']
+    regex_compiled = re.compile(package_regex)
 
     url = "https://firestore.googleapis.com/v1/projects/acme-register/databases/(default)/documents/packages/"
 
-    documents = requests.get(url).json()
+    documents = requests.get(url).json()["documents"]
     
     matching_packages = []
     for document in documents:
         package_name = document['fields']['name']['stringValue']
         package_version = document['fields']['version']['stringValue']
-        package_id = document['fields']['id']['stringValue']
 
-        if nameMatchesRegex(package_name, package_regex):
-            matching_packages.append({"Name" : package_name, "Version" : package_version, "ID" : package_id})
+        if regex_compiled.match(package_name) != None:
+            matching_packages.append({"Version" : package_version, "Name" : package_name})
 
-    proxy_integration_response = {
-        "isBase64Encoded": False,
-        "statusCode": 200,
-        "headers": {},
-        "body": json.dumps(matching_packages),
-    }
-
-    return proxy_integration_response
-  
-
-def nameMatchesRegex(name, regex):
-    expression = re.compile(regex)
-    return expression.match(name) != None
+    did_find_packages = len(matching_packages) > 0
+    if did_find_packages:
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps(matching_packages)
+        }
+    else:
+        return {
+            "statusCode": 404,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "error": "No package found under this regex."
+            })
+        }
+        
+    
